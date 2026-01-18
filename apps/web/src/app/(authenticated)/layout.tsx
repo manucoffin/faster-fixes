b1c7fc0c81e0a0357d2325e9ed059044bed5f776
Manuel Coffin <manuel.coffin@gmail.com>
2026-01-18 18:34:14 +0100

    feat(web): add theme toggle to authenticated layout header

--- apps/web/src/app/(authenticated)/layout.tsx
+++ apps/web/src/app/(authenticated)/layout.tsx
@@ -3,6 +3,7 @@ import { LayoutParams } from "@/types/next";
 import { SidebarInset, SidebarProvider, SidebarTrigger } from "@workspace/ui/components/sidebar";
 import { headers } from "next/headers";
 import { unauthorized } from "next/navigation";
+import { ThemeToggle } from "../_features/core/header/theme-toggle.client";
 import { AuthenticatedSidebar } from "./_features/sidebar/authenticated-sidebar.server";
 
 export default async function AuthenticatedLayout({ children }: LayoutParams) {
@@ -18,10 +19,12 @@ export default async function AuthenticatedLayout({ children }: LayoutParams) {
     <AuthenticatedSidebar />
 
     <SidebarInset>
-
-
-      <main>
+      <header className="flex px-4 py-2 items-center justify-between">
         <SidebarTrigger />
+        <ThemeToggle variant="ghost" size="icon" />
+      </header>
+
+      <main className="p-4">
         {children}
       </main>
     </SidebarInset>
