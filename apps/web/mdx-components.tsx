3d4fc7aa3935d29ac98d8f448482756a58501b66
Manuel Coffin <manuel.coffin@gmail.com>
2026-03-21 18:19:44 +0100

    feat(mdx): add remark-gfm and map HTML table elements to styled components

--- apps/web/mdx-components.tsx
+++ apps/web/mdx-components.tsx
@@ -1,7 +1,21 @@
 import type { MDXComponents } from "mdx/types";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@workspace/ui/components/table";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    table: (props) => <Table {...props} />,
+    thead: (props) => <TableHeader {...props} />,
+    tbody: (props) => <TableBody {...props} />,
+    tr: (props) => <TableRow {...props} />,
+    th: (props) => <TableHead {...props} />,
+    td: (props) => <TableCell {...props} />,
     ...components,
   };
 }
